b48a2e0f1799c0fba4088ac2a902cd143706bb6f
yanaph <fan37yana@gmail.com>
2022-10-07 09:38:05 +0200

    mistakes correction

--- src/main/java/ua/fan/hw7/Main.java
+++ src/main/java/ua/fan/hw7/Main.java
@@ -2,11 +2,9 @@ package ua.fan.hw7;
 
 import ua.fan.hw7.model.Vector;
 
-import java.util.Random;
-
 public class Main {
     public static void main(String[] args) {
-        Vector[] vectors = generateArrayOfVectors(2);
+        Vector[] vectors = Vector.generateArrayOfVectors(2);
         printArrayOfVectors(vectors);
         printCalculations(vectors);
     }
@@ -18,6 +16,7 @@ public class Main {
         }
 
         System.out.printf("---> DOT PRODUCT: %f%n",vectors[0].calculateDotProduct(vectors[1]));
+        System.out.printf("---> CROSS PRODUCT: %s",formatVectorsCoordinates(vectors[0].calculateCrossProduct(vectors[1])));
         System.out.printf("---> COS OF ANGLE: %f%n",vectors[0].calculateCosOfAngleBetweenVectors(vectors[1]));
         System.out.print("---> ADDITION: " + formatVectorsCoordinates(vectors[0].calculateAdditionOfVectors(vectors[1])));
         System.out.print("---> SUBTRACTION: " + formatVectorsCoordinates(vectors[0].calculateSubtractionOfVectors(vectors[1])));
@@ -32,14 +31,5 @@ public class Main {
 
     private static String formatVectorsCoordinates(Vector vector) {
         return String.format("x = %5.2f, y = %5.2f, z = %5.2f%n", vector.getX(), vector.getY(),vector.getZ());
-    }
-
-    private static Vector[] generateArrayOfVectors(int N) {
-        Vector[] array = new Vector[N];
-        Random random = new Random();
-        for (int i = 0; i < array.length; i++) {
-            array[i] = new Vector(random.nextDouble(20)-10, random.nextInt(20)-10, random.nextInt(20)-10);
-        }
-        return array;
     }
 }
--- src/main/java/ua/fan/hw7/model/Vector.java
+++ src/main/java/ua/fan/hw7/model/Vector.java
@@ -1,5 +1,7 @@
 package ua.fan.hw7.model;
 
+import java.util.Random;
+
 public class Vector {
     private double x;
     private double y;
@@ -36,11 +38,24 @@ public class Vector {
         return calculateDotProduct(b) / (calculateLength() * b.calculateLength());
     }
 
+    public Vector calculateCrossProduct(Vector b) {
+        return new Vector(y * b.getZ() - b.getY() * z, b.getX() * z - x * b.getZ(), x * b.getY() - b.getX() * y);
+    }
+
     public Vector calculateAdditionOfVectors(Vector b) {
         return new Vector(x + b.getX(), y + b.getY(), z + b.getZ());
     }
 
-    public Vector calculateSubtractionOfVectors(Vector b){
+    public Vector calculateSubtractionOfVectors(Vector b) {
         return new Vector(x - b.getX(), y - b.getY(), z - b.getZ());
+    }
+
+    public static Vector[] generateArrayOfVectors(int n) {
+        Vector[] array = new Vector[n];
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = new Vector(random.nextDouble(20) - 10, random.nextInt(20) - 10, random.nextInt(20) - 10);
+        }
+        return array;
     }
 }
