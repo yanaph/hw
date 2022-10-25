f1d934103bee8d77abdb701f837ac5622c0e32b8
yanaph <fan37yana@gmail.com>
2022-10-25 19:21:59 +0200

    hw11 part 1

--- src/main/java/ua/fan/hw10/entity/Student.java
+++ src/main/java/ua/fan/hw10/entity/Student.java
@@ -1,5 +1,7 @@
 package ua.fan.hw10.entity;
 
+import java.util.Objects;
+
 public class Student {
     private final String firstName;
     private final String lastName;
@@ -36,5 +38,18 @@ public class Student {
         } else {
             return 80;
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Double.compare(student.averageMark, averageMark) == 0 && Objects.equals(firstName, student.firstName) && Objects.equals(lastName, student.lastName) && Objects.equals(group, student.group);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, group, averageMark);
     }
 }
