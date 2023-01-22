637b80a4fb9fdd9e5e699d22afe23cef1f0a06bd
yanaph <fan37yana@gmail.com>
2023-01-22 01:57:27 +0100

    cleaning code and deleting Horse.java

--- src/main/java/ua/fan/hw32/model/Race.java
+++ src/main/java/ua/fan/hw32/model/Race.java
@@ -16,11 +16,8 @@ public class Race {
     private static final int TOTAL_DISTANCE = 1000;
     private static final Random RANDOM = new Random();
     private static final Object LOCK = new Object();
-
-    private static int horseNum = 0;
-    private Horse horse = new Horse(TOTAL_DISTANCE, RANDOM.nextInt(100, 200));
-    private List<Horse> racers = new ArrayList<>();
-    private List<Horse> results = new ArrayList<>();
+    private List<String> racers = new ArrayList<>();
+    private List<String> results = new ArrayList<>();
 
     public void startTheRace() {
         racing();
@@ -30,50 +27,35 @@ public class Race {
     }
 
     private void printResults() {
-        if (results.get(0).equals(getHorseByNumber(BET_HORSE))) {
+        if (results.get(0).equals(racers.get(BET_HORSE))) {
             System.out.println("Congratulations! Your horse won!");
         } else {
-            System.out.printf("Your horse took place #%d%n", results.indexOf(getHorseByNumber(BET_HORSE)));
+            System.out.printf("Your horse took place #%d%n", results.indexOf(racers.get(BET_HORSE)));
         }
 
         System.out.println("RESULT TABLE:");
         for (int i = 0; i < results.size(); i++) {
-            System.out.printf("#%d - Horse %d%n", (i + 1), horse.getHorseNumber());
+            System.out.printf("#%d - %s%n", (i + 1), racers.indexOf(results.get(i)) + 1);
         }
     }
 
     public void racing() {
+        String horseName = String.format("Horse-%d", racers.size() + 1);
+        racers.add(horseName);
+        int speed = RANDOM.nextInt(100, 200);
         int sleep = RANDOM.nextInt(400, 500);
-        increaseHorseNum();
-        horse.setHorseNumber(horseNum);
-        while (horse.getDistance_left() > 0) {
-            horseRunTheRace(sleep);
-        }
-        results.add(horse);
+        horseRunTheRace(TOTAL_DISTANCE, speed, sleep);
+        results.add(horseName);
     }
 
-    private void increaseHorseNum() {
-        synchronized (LOCK){
-            horseNum++;
-        }
-    }
-
-    private Horse getHorseByNumber(int betHorse) {
-        Horse horse = null;
-        for (int i = 0; i < racers.size(); i++) {
-            if (i + 1 == betHorse) {
-                horse = racers.get(i);
+    private void horseRunTheRace(int distance_left, int speed, int sleep) {
+        while (distance_left > 0) {
+            try {
+                distance_left -= speed;
+                Thread.sleep(sleep);
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
             }
-        }
-        return horse;
-    }
-
-    private void horseRunTheRace(int sleep) {
-        try {
-            horse.setDistance_left(horse.getDistance_left() - horse.getSpeed());
-            Thread.sleep(sleep);
-        } catch (InterruptedException e) {
-            System.out.println(e.getMessage());
         }
     }
 }
